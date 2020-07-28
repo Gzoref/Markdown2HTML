a90f4e5973d22bcc3b66f9c802f37bdbc84f4b75
Gzoref <gzoref@gmail.com>
2020-07-27 20:28:52 -0400

    fix error message

--- markdown2html.py
+++ markdown2html.py
@@ -8,11 +8,12 @@ from os import path
 Converts Markdown to HTML
 """
 
-def parse_headings(headings_list):
+
+def parse_headings(headings_list: list):
     """
     Parses markdown headings and converts them to HTML heading tags
     """
-    md_headings = []
+    html_headings = []
     number_of_hashes = 0
     for headings in headings_list:
         if len(headings) > 0 and headings[0] == '#':
@@ -25,13 +26,38 @@ def parse_headings(headings_list):
             headings = headings.strip('#')
             headings = headings.strip(' ')
             headings = '<' + html_tag + '>' + headings + '</' + html_tag + '>'
-        md_headings.append(headings)
-    return md_headings
+        html_headings.append(headings)
+    return html_headings
 
+
+def parse_unordered_lists(unorded_list: list) -> 'HTML ul li':
+    '''
+    Parses markdown unordered lists and converts them to HTML heading tags
+    '''
+    html_unordered = []
+    number_of_li = []
+    for uls in unorded_list:
+        if len(uls) > 0 and uls[0] == '-':
+            html_unordered.append('<ul>')
+            html_unordered.append('<li>' + uls[1:].strip(' ') + '</li>')
+        elif len(uls) == 0 or uls[0] != '-':
+            html_unordered.append('</ul>')
+            #html_unordered.append(uls)
+            print(unorded_list)
+                
+    return html_unordered
 
 def _main():
+    '''
+    Parses markdown file, makes appropriate method call
+    and creates HTML file with correct tags
+    '''
+
+    '''
+    Error messages
+    '''
     if len(argv) < 3:
-        print('Usage: ./markdown2html.py README.md', file=stderr)
+        print('Usage: ./markdown2html.py README.md README.html', file=stderr)
         exit(1)
     if not path.exists(argv[1]):
         print('Missing {}'.format(argv[1]), file=stderr)
@@ -39,16 +65,26 @@ def _main():
 
     markdown_file = argv[1]
     output_file = argv[2]
-    md_parser_list = []
-    md_text_list = []
 
+    # List of markdown file elements
+    md_parser_list = []
+
+    '''
+    Reads markdown file and parses content
+    '''
     with open(markdown_file) as file:
         md_parser_list = file.readlines()
     md_parser_list = ''.join(md_parser_list).split('\n')
 
     md_parser_list = parse_headings(md_parser_list)
     md_parser_list = '\n'.join(md_parser_list).split('\n')
+  
+    md_parser_list = parse_unordered_lists(md_parser_list)
+    md_parser_list = '\n'.join(md_parser_list).split('\n')
 
+    '''
+    Create HTML file and write converted markdown to tags
+    '''
     with open(output_file, 'w') as file:
         for md_symbol in md_parser_list:
             file.write(md_symbol + '\n')
